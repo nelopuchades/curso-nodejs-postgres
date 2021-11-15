a64ff227392d48148084c4b9d79fe48e2d484775
Nelo Puchades Bresó <correo@nelo.dev>
2021-11-15 23:13:36 +0100

    Using environment variables to handle data to connect to postgres

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
+const { config } = require('../config/config');
 
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'admin',
-    password: 'admin123',
-    database: 'my_store'
-});
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
